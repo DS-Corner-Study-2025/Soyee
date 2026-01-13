40bae4d96fc4303e36e8891e0f5644d16e420e52
soyee <hj37new@gmail.com>
2026-01-13 20:49:36 +0900

    13주차 스프링

---
+++ week13/BookMarket/src/main/java/com/springboot/controller/LoginController.java
@@ -0,0 +1,33 @@
+package com.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class LoginController {
+
+
+	@GetMapping("/login")
+	public String login(HttpServletRequest request) {   
+		String sessionid = request.getSession(true).getId();
+		System.out.println(sessionid);
+      return "login";        
+   }
+ 
+   
+	@GetMapping("/loginfailed")
+	public String loginerror(Model model) { 
+      model.addAttribute("error", "true");   
+      return "login";                      
+   }   
+	
+	@GetMapping("/logout")
+	public String logout(Model model) { 
+		return "login";  
+	}
+	
+}
